2c3f97b15b8f97de2d689da7c8bc7113f1dff692
itzy <itzy_90@hotmail.com>
2014-05-19 20:41:04 +0200

    Create test.php

---
+++ test.php
@@ -0,0 +1,33 @@
+
+<?php
+
+      //include the stylesheet
+    <link rel="stylesheet/less" type="text/css" href="css/gallery.less">
+?>
+
+    //create the gallery
+<div id="lightbox">
+    <ul>
+        <li>
+            <img src="img/picture1_thumb.jpg" data-large="img/picture1.jpg"/>
+            <div class="image_title">
+                <h5 class="title">Picture 1</h5>
+            </div>
+        </li>
+        <li>
+            <img src="img/picture2_thumb.jpg" data-large="img/picture2.jpg"/>
+            <div class="image_title">
+                <h5 class="title">Picture 2</h5>
+            </div>
+        </li>
+
+    </ul>
+</div>
+</div>
+
+<?php
+
+    //include javascript-files
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"&gt;&lt;/script>
+    <script src="js/gallery.js"&gt;&lt;/script>
+?>
