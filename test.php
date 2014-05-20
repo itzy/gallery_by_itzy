1cc3e8932b1826e6446d7b02d7052ca545972407
itzy <itzy_90@hotmail.com>
2014-05-20 07:44:53 +0200

    Update test.php
    working demo

--- test.php
+++ test.php
@@ -1,21 +1,19 @@
+<!-- include the stylesheet-->
+<link rel="stylesheet/less" type="text/css" href="css/gallery.less">
+<title>Gallery by itzy</title>
 
-<?php
 
-      //include the stylesheet
-    <link rel="stylesheet/less" type="text/css" href="css/gallery.less">
-?>
-
-    //create the gallery
+<!-- create the gallery-->
 <div id="lightbox">
     <ul>
         <li>
-            <img src="img/picture1_thumb.jpg" data-large="img/picture1.jpg"/>
+            <img src="img/gallery1_thumb.jpg" data-large="img/gallery1.jpg"/>
             <div class="image_title">
                 <h5 class="title">Picture 1</h5>
             </div>
         </li>
         <li>
-            <img src="img/picture2_thumb.jpg" data-large="img/picture2.jpg"/>
+            <img src="img/gallery2_thumb.jpg" data-large="img/gallery2.jpg"/>
             <div class="image_title">
                 <h5 class="title">Picture 2</h5>
             </div>
@@ -25,9 +23,7 @@
 </div>
 </div>
 
-<?php
-
-    //include javascript-files
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
-    <script src="js/gallery.js"&gt;&lt;/script>
-?>
+<!-- include javascript-files-->
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js" type="text/javascript"></script>
+<script src="js/gallery.js"></script>
+<script src="../less.min.js"></script>
