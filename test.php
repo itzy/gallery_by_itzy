d113da45fecc1ed2a0b0d07d48c432b79173eab5
itzy <itzy_90@hotmail.com>
2014-05-20 07:48:13 +0200

    Update test.php

--- test.php
+++ test.php
@@ -1,5 +1,5 @@
 <!-- include the stylesheet-->
-<link rel="stylesheet/less" type="text/css" href="css/gallery.less">
+<link rel="stylesheet" type="text/css" href="css/gallery.css">
 <title>Gallery by itzy</title>
 
 
@@ -7,13 +7,13 @@
 <div id="lightbox">
     <ul>
         <li>
-            <img src="img/gallery1_thumb.jpg" data-large="img/gallery1.jpg"/>
+            <img src="gallery_by_itzy/img/gallery1_thumb.jpg" data-large="gallery_by_itzy/img/gallery1.jpg"/>
             <div class="image_title">
                 <h5 class="title">Picture 1</h5>
             </div>
         </li>
         <li>
-            <img src="img/gallery2_thumb.jpg" data-large="img/gallery2.jpg"/>
+            <img src="gallery_by_itzy/img/gallery2_thumb.jpg" data-large="gallery_by_itzy/img/gallery2.jpg"/>
             <div class="image_title">
                 <h5 class="title">Picture 2</h5>
             </div>
@@ -26,4 +26,3 @@
 <!-- include javascript-files-->
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js" type="text/javascript"></script>
 <script src="js/gallery.js"></script>
-<script src="../less.min.js"></script>
